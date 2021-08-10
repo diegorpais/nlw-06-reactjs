3b0f011aa3b4480b4f430507b82ba8f469d3e819
Diego Raphael Borges Pais <draphae5@ford.com>
2021-08-10 16:34:20 -0300

    feat(newroom): criacao da pagina NewRoom estatica

---
+++ src/pages/NewRoom/index.tsx
@@ -0,0 +1,36 @@
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+
+import { Button } from '../../components/Button';
+
+import '../../assets/styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustracao simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&A ao-vivo</strong>
+        <p>Tire as duvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova Sala</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+            <p>
+              Quer entrar em uma sala existente? <a href="/">Clique aqui</a>
+            </p>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
