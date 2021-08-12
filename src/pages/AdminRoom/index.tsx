2e76969d8cb5a5e5e42806ade0a83e88a81f39c8
Diego Raphael Borges Pais <draphae5@ford.com>
2021-08-12 09:29:52 -0300

    feat(adminroom, app): pagina de sala de admin

---
+++ src/pages/AdminRoom/index.tsx
@@ -0,0 +1,104 @@
+import { useEffect } from 'react';
+import { FormEvent, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+
+import { Button } from '../../components/Button';
+import { Question } from '../../components/Question';
+import { RoomCode } from '../../components/RoomCode';
+
+import { useAuth } from '../../hooks/useAuth';
+import { useRoom } from '../../hooks/useRoom';
+import { database } from '../../services/firebase';
+
+import '../Room/styles.scss';
+
+type RoomParams = {
+  id: string;
+};
+
+
+
+export function AdminRoom() {
+
+  const { user } = useAuth();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const { title, questions } = useRoom(roomId);
+
+  const [newQuestion, setNewQuestion] = useState('');
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() == '') {
+      return;
+    }
+
+    if (!user) {
+      throw new Error('Você deve estar logado.');
+      /** Biblioteca para TOAST MESSAGE => react-hot-toast */
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar
+      },
+      isHighLighted: false,
+      isAnswered: false
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
+
+  };
+
+  return (
+    <div id="page-room">
+
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar Sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {
+            questions.length > 0 && <span>{questions.length} pergunta(s)</span>
+          }
+        </div>
+
+        <div className="question-list">
+          {
+            questions.map(question => {
+              return (
+                <Question
+                  key={question.id}
+                  content={question.content}
+                  author={question.author}
+                  isAnswered={question.isAnswered}
+                  isHighLighted={question.isHighLighted}
+                >
+
+
+                </Question>
+              );
+            })
+          }
+        </div>
+
+      </main>
+
+    </div>
+  );
+}
